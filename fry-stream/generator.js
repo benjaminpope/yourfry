b1fc19483307e21f9cb5098549460a264bf7763b
Clinton <clinton@slightlymore.co.uk>
2014-10-30 16:01:27 +0000

    fix typo

--- fry-stream/generator.js
+++ fry-stream/generator.js
@@ -8,7 +8,7 @@ var tweets = [
   "of this kind ought to have to confess this, moronic, puerile and cheap resonance of reality. So I did.",
   "which Rowan handed to Adrian to forget his lines. We will bring you something.' 'Good-o,' said Adrian.",
   "remember the first night of the lips, the angle of the people in the grip of what was the first floor.",
-  "utter despair."
+  "utter despair.",
   "fooling Cromie. He still needed to clear of me. I ought to be able to continue. ‘Well, go on,’ said Ignaz.",
   "picture and priceless biographical information: ‘John hates marmalade but Ringo is very good friends.",
   "and returned to his door and work without troubling myself too drastically for such was the first floor.",
